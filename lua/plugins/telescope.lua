49777680fd5af3db7670deed31e58267bcef0f6c
DatNguyen <datnt@piscale.com>
2025-08-11 09:21:36 +0700

    update: telescope show hidden

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,15 +38,18 @@ function M.setup()
           preview_width = 0.6,
         },
       },
+      find_files = {
+        hidden = true,
+      },
     },
   }
-  
+
   -- Enable telescope fzf native, if installed
   pcall(require('telescope').load_extension, 'fzf')
-  
+
   -- Telescope keymaps
   local builtin = require('telescope.builtin')
-  
+
   vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
   vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = '[ ] Find existing buffers' })
   vim.keymap.set('n', '<leader>/', function()
@@ -55,7 +58,7 @@ function M.setup()
       previewer = false,
     })
   end, { desc = '[/] Fuzzily search in current buffer' })
-  
+
   vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
   vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
