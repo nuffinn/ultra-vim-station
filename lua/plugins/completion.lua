fc7167368e15c4449af84fc7dcfa4fe8752990cb
datnt <datnt@piscale.com>
2025-10-19 17:36:08 +0700

    feat:add back tab to iterate through list of lsp completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -11,7 +11,7 @@ function M.setup()
         luasnip.lsp_expand(args.body)
       end,
     },
-    
+
     mapping = cmp.mapping.preset.insert {
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -20,12 +20,36 @@ function M.setup()
         behavior = cmp.ConfirmBehavior.Replace,
         select = true,
       },
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          local entry = cmp.get_selected_entry()
+          if entry and entry.source.name == 'copilot' then
+            cmp.confirm({ select = true })
+          else
+            cmp.select_next_item()
+          end
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { 'i', 's' }),
     },
-    
+
+
     sources = {
-      { name = 'copilot', group_index = 2 },
+      { name = 'copilot',  group_index = 2 },
       { name = 'nvim_lsp', group_index = 2 },
-      { name = 'luasnip', group_index = 2 },
+      { name = 'luasnip',  group_index = 2 },
     },
   }
 end
